e9c1bdb727df8ca2d3408dd24ab4a4ee1bcb9f20
Jaymes Waters and Brad Grzesiak <dev+jaym3s+listrophy@bendyworks.com>
2012-07-02 15:16:51 -0500

    Refactor tests to reflect simplified coffeescript.rb

--- spec/lib/bwoken/coffeescript_spec.rb
+++ spec/lib/bwoken/coffeescript_spec.rb
@@ -6,25 +6,49 @@ require 'spec_helper'
 describe Bwoken::Coffeescript do
   let(:subject) { Bwoken::Coffeescript }
 
-  describe '#compile' do
-    it "compiles js to coffeescript" do
-      subject.stub(:source_contents => 'a = 1')
-      subject.compile.should match /var/
+  describe '.precompile' do
+    let(:test_coffee) {"foo = 1\n#import bazzle.js\nbar = 2"}
+    it 'splits #import statements from other statements' do
+      subject.precompile(test_coffee).should == ["#import bazzle.js\n", "foo = 1\nbar = 2"]
     end
   end
 
-  describe '#capture_imports raw_javascript' do
-    let(:test_js) {"var foo;\n#import bazzle.js\nvar bar;"}
-    it "collects the #import tag" do
-      subject.capture_imports(test_js)
-      subject.import_strings.should == ["#import bazzle.js"]
+  describe '.compile' do
+    it 'precompiles' do
+      subject.should_receive(:precompile)
+      IO.stub(:read)
+      subject.context.stub(:call)
+      subject.stub(:write)
+      subject.compile 'a', 'b'
+    end
+
+    it 'cofffeescript-compiles' do
+      subject.stub(:precompile)
+      IO.stub(:read)
+      subject.context.should_receive(:call)
+      subject.stub(:write)
+      subject.compile 'a', 'b'
+    end
+
+    it 'writes to disk' do
+      subject.stub(:precompile)
+      IO.stub(:read)
+      subject.context.stub(:call)
+      subject.should_receive(:write)
+      subject.compile 'a', 'b'
     end
   end
 
-  describe '#remove_imports raw_javascript' do
-    let(:test_js) {"var foo;\n#import bazzle.js\nvar bar;"}
-    it 'removes the #import tag' do
-      subject.remove_imports(test_js).should == "var foo;\n\nvar bar;"
+  describe '.write' do
+    it 'saves the javascript to the destination_file' do
+      stringio = StringIO.new
+      destination_file = 'bazzle/bar.js'
+
+      File.should_receive(:open).with(destination_file, 'w').and_yield(stringio)
+
+      subject.write('foo', '', 'bar', :to => destination_file)
+
+      stringio.string.should == "foo\nbar\n"
     end
   end
 
